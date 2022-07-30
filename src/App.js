1eb4d6bb3f852c2a05885e4afd0ca7a8dad98585
Jason Watkins <goushoryuken@gmail.com>
2022-07-29 18:24:01 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header><h1>memory card</h1></header>
+      <main>
+        <div className="card-area">
+          <div id="zero" className="card"></div>
+          <div id="one" className="card"></div>
+          <div id="two" className="card"></div>
+          <div id="three" className="card"></div>
+          <div id="four" className="card"></div>
+          <div id="five" className="card"></div>
+          <div id="six" className="card"></div>
+          <div id="seven" className="card"></div>
+          <div id="eight" className="card"></div>
+          <div id="nine" className="card"></div>
+          <div id="ten" className="card"></div>
+          <div id="eleven" className="card"></div>
+          <div id="twelve" className="card"></div>
+          <div id="thirteen" className="card"></div>
+          <div id="fourteen" className="card"></div>
+          <div id="fifteen" className="card"></div>
+          <div id="sixteen" className="card"></div>
+          <div id="seventeen" className="card"></div>
+          <div id="eighteen" className="card"></div>
+          <div id="nineteen" className="card"></div>
+          <div id="twenty" className="card"></div>
+          <div id="twentyone" className="card"></div>
+        </div>
+      </main>
     </div>
   );
 }
