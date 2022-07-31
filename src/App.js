bd0a54c90ea6ba96f91facc30a61374a54475f5f
Jason Watkins <goushoryuken@gmail.com>
2022-07-31 13:11:50 -0700

    initial styling

--- src/App.js
+++ src/App.js
@@ -2,31 +2,83 @@
 function App() {
   return (
     <div className="App">
-      <header><h1>memory card</h1></header>
+      <header>
+        <h1>memory card</h1>
+        <div className="score-box">
+          <div className="current">current score</div>
+          <div className="high">high score</div>
+          <div className="about">about</div>
+        </div>
+        
+      </header>
       <main>
         <div className="card-area">
-          <div id="zero" className="card"></div>
-          <div id="one" className="card"></div>
-          <div id="two" className="card"></div>
-          <div id="three" className="card"></div>
-          <div id="four" className="card"></div>
-          <div id="five" className="card"></div>
-          <div id="six" className="card"></div>
-          <div id="seven" className="card"></div>
-          <div id="eight" className="card"></div>
-          <div id="nine" className="card"></div>
-          <div id="ten" className="card"></div>
-          <div id="eleven" className="card"></div>
-          <div id="twelve" className="card"></div>
-          <div id="thirteen" className="card"></div>
-          <div id="fourteen" className="card"></div>
-          <div id="fifteen" className="card"></div>
-          <div id="sixteen" className="card"></div>
-          <div id="seventeen" className="card"></div>
-          <div id="eighteen" className="card"></div>
-          <div id="nineteen" className="card"></div>
-          <div id="twenty" className="card"></div>
-          <div id="twentyone" className="card"></div>
+          <div className="card-container"> 
+            <div id="zero" className="card"></div>
+          </div>
+          <div className="card-container">    
+            <div id="one" className="card"></div>
+          </div> 
+          <div className="card-container">   
+            <div id="two" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="three" className="card"></div>
+          </div> 
+          <div className="card-container">    
+            <div id="four" className="card"></div>
+          </div> 
+          <div className="card-container">
+            <div id="five" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="six" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="seven" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="eight" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="nine" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="ten" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="eleven" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="twelve" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="thirteen" className="card"></div>
+          </div> 
+          <div className="card-container">
+            <div id="fourteen" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="fifteen" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="sixteen" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="seventeen" className="card"></div>
+          </div>   
+          <div className="card-container">  
+            <div id="eighteen" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="nineteen" className="card"></div>
+          </div> 
+          <div className="card-container">  
+            <div id="twenty" className="card"></div>
+          </div>   
+          <div className="card-container">  
+            <div id="twentyone" className="card"></div>
+          </div>  
         </div>
       </main>
     </div>
